26d4b8208bc53f28c994ae98566a8474b2044259
student202010 <77183724+student202010@users.noreply.github.com>
2021-01-09 07:38:05 +0300

    Add files via upload
    Testing Example - Borrowing Library

---
+++ borrowinglibrary1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Neighborhood Borrowing Library</title>
+</head>
+<body>
+
+
+{
+	"Books": {
+		"book": [
+			{
+				"serialnumber": "2020_001",
+				"booktitle": "A Christmas Carol",
+				"bookauthor": "Charles Dickens",
+				"publishdate": "19/12/1843",
+				"availability": false
+							},
+			{
+				"serialnumber": "2020_002",
+				"booktitle": "The Lord Of The Rings",
+				"bookauthor": "J. J. Tolkien",
+				"publishdate": "29/07/1954",
+				"availability": true
+			},
+			{
+				"serialnumber": "2020_003",
+				"booktitle": "The Last Of The Mohicans",
+				"bookauthor": "James Cooper",
+				"publishdate": "13/02/1826",
+				"availability": false
+			},
+			{
+				"serialnumber": "2020_004",
+				"booktitle": "Alice In Wonderland",
+				"bookauthor": "Lewis Carroll",
+				"publishdate": "26/11/1865",
+				"availability": true
+			},
+			{
+				"serialnumber": "2020_005",
+				"booktitle": "A Little Princess",
+				"bookauthor": "Frances Brunett",
+				"publishdate": "06/09/1905",
+				"availability": true
+			}
+		]
+	}
+}
+
+
+</body
+</html>
